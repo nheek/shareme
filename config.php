98a9bf773bd2b1791b28616524d6f83a18126655
Nick <83167470+nheek@users.noreply.github.com>
2024-01-26 22:45:23 +0100

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sqlHost = "localhost";
-$sqlUser = "your-sql-username";
-$sqlPass = "your-sql-password";
-$sqlName = "your-sql-database-name";
-
-$sqlConnect = new mysqli($sqlHost, $sqlUser, $sqlPass, $sqlName);
