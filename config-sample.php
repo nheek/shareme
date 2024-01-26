833811a762e663f748affde3a06d3ee1eb75e4f3
nheek <nickjameshipol@gmail.com>
2024-01-26 22:43:10 +0100

    added .htaccess

---
+++ config-sample.php
@@ -0,0 +1,7 @@
+<?php
+$sqlHost = "localhost";
+$sqlUser = "your-sql-username";
+$sqlPass = "your-sql-password";
+$sqlName = "your-sql-database-name";
+
+$sqlConnect = new mysqli($sqlHost, $sqlUser, $sqlPass, $sqlName);
